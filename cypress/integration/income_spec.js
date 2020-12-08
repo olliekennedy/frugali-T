6ff2ad6657c5ba548b7994291e166cee2f6d146f
Eunice Kontor <Eunice.Kontor@ft.com>
2020-12-08 17:56:56 +0000

    created output log - just works for one category

--- cypress/integration/income_spec.js
+++ cypress/integration/income_spec.js
@@ -7,3 +7,23 @@ context('Check salary', () =>{
     cy.get('#net-output').should('have.text', '£100000')
   })
 });
+
+context('Check outgoings', () =>{
+  beforeEach(() => {
+  cy.visit("/")
+  })
+  it('outgoings', function() {
+    cy.get("#income-input").type('100000');
+    cy.get('#bills-input').type('10000');
+    cy.get('#groceries-input').type('10000');
+    cy.get('#travel-input').type('10000');
+    cy.get('#entertainment-input').type('10000');
+    cy.get('#hobbies-input').type('10000');
+    cy.get('#tea-input').type('10000');
+    cy.get('#loans-input').type('10000');
+    cy.get('#savings-input').type('10000');
+    cy.get('#outgoings-output').should('have.text', '£90000')
+  })
+});
+
+
--- static/js/interface.js
+++ static/js/interface.js
@@ -1,16 +1,44 @@
+// const input = document.querySelector('input');
+// const log = document.getElementById('net-output');
+// const totalOutgoings = document.getElementById('groceries-input');
+// console.log(totalOutgoings);
+// // input.addEventListener('input', updateValue);
+// let inputs = document.querySelectorAll("input");
+// inputs.forEach(function(elem) {
+//     elem.addEventListener("input", updateValue);
+// });
+// function updateValue(e) {
+//     log.textContent = `£${e.target.value}`;
+// }
 const input = document.querySelector('input');
+const outgoings = document.querySelector("#outgoings")
 const log = document.getElementById('net-output');
-const totalOutgoings = document.getElementById('groceries-input');
-// console.log(totalOutgoings)
-// input.addEventListener('input', updateValue);
-let inputs = document.querySelectorAll("input");
-inputs.forEach(function(elem) {
-    elem.addEventListener("input",updateValue);
-});
+const outG = document.getElementById('outgoings-output');
+
+input.addEventListener('input', updateValue);
 function updateValue(e) {
-  if (totalOutgoings.value == NaN ) {
-    log.textContent = `£${parseInt(document.getElementById("income-input").value)}`;
-  } else {
-    log.textContent = `£${parseInt(document.getElementById("income-input").value)- parseInt(totalOutgoings.value)}`;
-  }
-}
+  log.textContent = `£${e.target.value}`;
+  // console.log(checking());
+};
+
+outgoings.addEventListener('input', updateOutgoing);
+function updateOutgoing(e) {
+  outG.textContent = `£${e.target.value}`;
+};
+
+// function outgoingValue(){
+//   const sum = bills.value;
+//   document.getElementById("outgoings-output").textContent = `£${sum}`;
+// };
+
+// const bills = document.getElementById('bills-input');
+// const groceries = document.getElementById('groceries-input');
+// const travel = document.getElementById('travel-input');
+// const loans = document.getElementById('loans-input');
+// const hobbies = document.getElementById('hobbies-input');
+// const tea = document.getElementById('tea-input');
+// const savings = document.getElementById('savings-input');
+// const entertainment = document.getElementById('entertainment-input');
+
+
+
