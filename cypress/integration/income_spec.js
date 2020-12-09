d4430fad02bd8ee24c2a6ed4133dfcecabf183f1
olliekennedy <ollie.kennedy1@gmail.com>
2020-12-09 14:12:43 +0000

    changed text inputs to number inputs

--- cypress/integration/income_spec.js
+++ cypress/integration/income_spec.js
@@ -6,4 +6,8 @@ context('Check salary', () =>{
     cy.get("#income-input").type('100000');
     cy.get('#net-output').should('have.text', '£100000')
   })
+  it('user cannot input letters', function() {
+    cy.get("#income-input").type('oanevn');
+    cy.get('#net-output').should('have.text', '£0')
+  })
 });
