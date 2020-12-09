3e2f3cc32c3aad390ba60dd42a8fbb94922e0ead
olliekennedy <ollie.kennedy1@gmail.com>
2020-12-09 08:09:52 +0000

    change / add tests

--- cypress/integration/income_spec.js
+++ cypress/integration/income_spec.js
@@ -1,29 +1,9 @@
 context('Check salary', () =>{
   beforeEach(() => {
-  cy.visit("/")
+    cy.visit("/")
   })
-  it('user can input their salary', function() {
+  it('user can input their salary and net increase by that amount', function() {
     cy.get("#income-input").type('100000');
     cy.get('#net-output').should('have.text', '£100000')
   })
 });
-
-context('Check outgoings', () =>{
-  beforeEach(() => {
-  cy.visit("/")
-  })
-  it('outgoings', function() {
-    cy.get("#income-input").type('100000');
-    cy.get('#bills-input').type('10000');
-    cy.get('#groceries-input').type('10000');
-    cy.get('#travel-input').type('10000');
-    cy.get('#entertainment-input').type('10000');
-    cy.get('#hobbies-input').type('10000');
-    cy.get('#tea-input').type('10000');
-    cy.get('#loans-input').type('10000');
-    cy.get('#savings-input').type('10000');
-    cy.get('#outgoings-output').should('have.text', '£90000')
-  })
-});
-
-
