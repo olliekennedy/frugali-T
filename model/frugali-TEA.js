068184ce53826a99c07744ba657539f96b6d0e1a
Hatairy <fanionayo@gmail.com>
2020-12-10 10:43:10 +0000

    start of dbs

---
+++ model/frugali-TEA.js
@@ -0,0 +1,11 @@
+const budgetSchema = new mongoose.Schema({})
+Bills: number,
+Entertainment: number,
+Travel: number,
+Loans: number,
+Tea: number,
+Hobbies: number,
+Savings: number,
+});
+var User = mongoose.model('budget', budgetSchema);
+module.exports = Budget;
