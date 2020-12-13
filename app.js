4cf8d5ae28037891fef5088cb75abf0cedaae4c5
Eunice Kontor <Eunice.Kontor@ft.com>
2020-12-13 12:53:36 +0000

    routes set - app file made lightweght

--- app.js
+++ app.js
@@ -1,51 +1,30 @@
 const express = require('express');
+const router = express.Router();
 const app = express();
 const ejs = require('ejs');
 const port = 3000;
 const mongoose = require('mongoose');
+const expressEjsLayout = require('express-ejs-layouts');
+
+// mongoose
 mongoose.connect('mongodb://localhost/frugali_TEA', {useNewUrlParser: true});
-//var MongoClient = require('mongodb').MongoClient
+
 const db = mongoose.connection
-//MongoClient.connect("mongodb://localhost:27017/test", function (err, db) {
-//collection.insert({ id: 1,"Bills" : 100, "Groceries" : 250, "Travel" : 150, "Loans" : 70, "Hobbies" : 80, "Tea" : 50, "Savings" : 400, "Entertainment" : 30});
-//});
+
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
 console.log ("we're connected!")
 });
+
 app.set('view engine', 'ejs');
-
-// app.configure(function(){
-
-// });
 app.use("/", express.static('./'));
+// app.use(expressEjsLayout);
 
-//Redefined routes - can be put in the router directory to keep app.js clean
-app.get('/', (req, res) => {
-  res.render('homepage');
-})
+app.use(express.urlencoded({extended : false}));
 
-app.get('/account', (req, res) => {
-  res.render('account');
-})
-
-app.get('/signup', (req, res) => {
-  res.render('signup');
-});
-
-app.get("/signin", (req, res) => {
-  res.render('signin');
-})
-
-app.post('/signup', (req, res) => {
-})
-app.post('/signin', (req, res) => {
-})
-
-
-
+app.use('/', require('./routes/index.js'));
 
 console.log('test')
 app.listen(port, () => {
   console.log(`App listening on: ${port}`);
-})
+});
---
+++ routes/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('homepage');
+})
+
+router.get('/signup', (req, res) => {
+  res.render('signup');
+})
+
+router.get('/account', (req, res) => {
+  res.render('account');
+})
+
+router.get("/signin", (req, res) => {
+  res.render('signin');
+})
+
+
+module.exports = router;
