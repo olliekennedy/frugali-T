6eb18e799819ea7d87dc6a59dd0e4610e25b9829
Grace Farren <grace_farren@yahoo.co.uk>
2020-12-15 12:05:28 +0000

    database connect all ok, shows information and can add to db

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-const app = express();
-const ejs = require('ejs');
-const port = 3000;
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
+// monk connection
+var monk = require('monk');
+var db = monk('localhost:27017/frugali_TEA')
 
-// mongoose connection
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/frugali_TEA', {useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection
-// console.log("I am a db and this is my jam" + db)
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  });
+// // mongoose connection
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/frugali_TEA', {useNewUrlParser: true, useUnifiedTopology: true });
+// const db = mongoose.connection
+// // console.log("I am a db and this is my jam" + db)
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   });
 
-// // monk connection
-// var monk = require('monk');
-// var dbMonk = monk('localhost:27017/frugali_TEA')
+var indexRouter = require('./routes/index');
+var transactionsRouter = require("./routes/transactions");
 
-
-app.use(express.urlencoded({ extended: false }));
+var app = express();
 
 //  view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-var transactionsRouter = require("./routes/transactions")
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'static')));
 
 // Make our db accessible to our router
 app.use(function(req, res, next){
@@ -36,15 +39,23 @@ app.use(function(req, res, next){
 });
 
 // Routes
-app.use("/", express.static('./'));
-app.get('/', (req, res) => {
-  res.render('index');
-})
+app.use("/", indexRouter);
 app.use("/transactions", transactionsRouter);
 
-// console.log('test')
-app.listen(port, () => {
-  console.log(`App listening on: ${port}`);
-})
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
--- routes/transactions.js
+++ routes/transactions.js
@@ -1,37 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var Transaction = require("../model/transaction");
+// var Transaction = require("../model/transaction");
 
 router.get('/', function(req, res, next) {
   res.render('newtransaction', { title: "Transactions" });
 })
-
-// router.post('/new', function(req, res) {
-//   var db = req.db;
-//   console.log(db)
-//
-//   var description = req.body.description;
-//   var category = req.body.category;
-//   var amount = req.body.amount;
-//
-//   var collection = db.get('transactioncollection');
-//
-//   collection.insert({
-//       "description" : description,
-//       "category" : category,
-//       "amount" : amount
-//   }, function (err, doc) {
-//       if (err) {
-//           // If it failed, return error
-//           res.send("There was a problem adding the information to the database.");
-//       }
-//       else {
-//           // And forward to success page
-//           res.redirect("newtransaction");
-//       }
-//   });
-//   res.redirect("/list")
-// });
 
 router.get('/list', function(req, res) {
   var db = req.db;
@@ -41,9 +14,32 @@ router.get('/list', function(req, res) {
           "transactions" : docs
       });
   });
-  // res.render('transactionlist', {
-  //     "transactions" : collection.find({})
-  // });
-})
+  })
+
+router.post('/list', function(req, res) {
+  var db = req.db;
+
+  var description = req.body.description;
+  var category = req.body.category;
+  var amount = req.body.amount;
+
+  var collection = db.get('transaction');
+
+  collection.insert({
+      "description" : description,
+      "category" : category,
+      "amount" : amount
+  }, function (err, doc) {
+      if (err) {
+          // If it failed, return error
+          res.send("There was a problem adding the information to the database.");
+      }
+      else {
+          // And forward to success page
+          res.redirect('/transactions/list')
+      }
+  });
+});
+
 
 module.exports = router;
